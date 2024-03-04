ad39768f04ca7325a63039255246ea234270a21f
mohammed <mohammedfuta@gmail.com>
2024-03-04 21:16:20 +0000

    isBlockValid function is updated

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func isBlockValid(newBlock, oldBlock Block) bool {
 	if oldBlock.Index+1!=newBlock.Index{
 		return false
 	}
-	if oldBlock.hash != newBlock.PrevHash{
+	if oldBlock.Hash != newBlock.PrevHash{
 		return false
 	}
 	if calculateHash(newBlock)!=newBlock.Hash{
